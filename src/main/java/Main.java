a04f6ea1d0b04dd3738406d31b6d93cf1f9dba17
Jarrett Atkinson <jj_atkinson@att.net>
2015-12-08 11:40:35 -0600

    Fix getPlayers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,9 @@ import connectn.game.Runner;
 /**
  * Created by Jarrett on 12/05/15.
  */
-public class ConnectNController {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Starting simulation");
-        Runner.runGames();
+        new Runner().runGames();
     }
 }
